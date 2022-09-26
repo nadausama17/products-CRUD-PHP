fc90215d41b0c85baa06959fc828341979d1758d
nada <nadaua2@gmail.com>
2022-09-26 23:10:33 +0200

    put the files that the user can access in public folder

---
+++ public/products/delete_product.php
@@ -0,0 +1,26 @@
+<?php
+    $pdo = require_once '../../database.php';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $id = $_POST['id'] ?? null;
+
+        if(!$id){
+            header('Location: index.php');
+        }
+
+        $statement = $pdo->prepare('select * from products where id= :id');
+        $statement-> bindValue(':id',$id);
+        $statement -> execute();
+        $product = $statement ->fetch(PDO::FETCH_ASSOC);
+        unlink('../images/'.$product['image']);
+        rmdir('../images/'.dirname($product['image']));
+    
+        $statement = $pdo ->prepare('delete from products where id= :id');
+        $statement-> bindValue(':id',$id);
+        $statement -> execute();
+
+        header('Location: index.php');
+    }else{
+        header('Location: index.php');
+    }
+?>
